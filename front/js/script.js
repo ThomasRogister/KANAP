372854338b8aafbc1848faf05f219896319071a7
thomas.rogister <thomasrogister@protonmail.com>
2022-08-16 10:45:26 +0200

    script js ok

--- front/js/script.js
+++ front/js/script.js
@@ -1,41 +1,40 @@
 
 
-// FUNCTION - RÉCUPÉRATION DES PRODUITS DEPUIS L'API
-function getArticles() {
-    fetch("http://localhost:3000/api/products")
-        // DEMANDE DE RECEVOIR LA REPONSE EN J.SON
-        .then((res) => res.json())
-        // LA RÉPONSE JSON SERA APPELÉ "ARTICLES"
-        .then((articles) => {
-            //RENVOIE DES INFORMATION ARTICLE DANS LA CONSOLE SOUS FORME DE TABLEAU
-            console.table(articles);
-            //APPEL DE LA FUNCTION D'AFFICHAGE 
-            products(articles);
-        })
+//  RÉCUPÉRATION DES PRODUITS DEPUIS L'API
 
-        // AFFICHAGE D'UN MESSAGE EN CAS D'ERREUR
-        .catch((err) => {
-            document
-                .querySelector("#items")
-                .innerHTML = "<h2>Ce site est en maintenance,<br> nous nous excusons pour la gêne occasionné.</h2>"
+fetch("http://localhost:3000/api/products")
+    // DEMANDE DE RECEVOIR LA REPONSE EN J.SON
+    .then((res) => res.json())
+    // LA RÉPONSE JSON SERA APPELÉ "ARTICLES"
+    .then((articles) => {
+        //RENVOIE DES INFORMATION ARTICLE DANS LA CONSOLE SOUS FORME DE TABLEAU
+        console.table(articles);
+        //APPEL DE LA FUNCTION D'AFFICHAGE 
+        products(articles);
+    })
 
-        });
+    // AFFICHAGE D'UN MESSAGE EN CAS D'ERREUR
+    .catch((err) => {
+        document
+            .querySelector("#items")
+            .innerHTML = "<h2>Ce site est en maintenance,<br> nous nous excusons pour la gêne occasionné.</h2>"
+        console.log("err + empêche la réponse de s'afficher")
 
-    // FUNCTION - AFFICHAGE DES ARTICLES SUR L'INDEX.HTML
-    function products(index) {
-        let sectionItems = document
-            .querySelector("#items");
-        // BOUCLE POUR RÉCUPÉRATION DE CHAQUE ARTICLE
-        for (let article of index) {
-            sectionItems.innerHTML += `<a href="./product.html?_id=${article._id}">
+    });
+
+// FUNCTION - AFFICHAGE DES ARTICLES SUR L'INDEX.HTML
+function products(index) {
+    let sectionItems = document
+        .querySelector("#items");
+    // BOUCLE POUR RÉCUPÉRATION DE CHAQUE ARTICLE
+    for (let article of index) {
+        sectionItems.innerHTML += `<a href="./product.html?_id=${article._id}">
                 <article>
-                    <img scr="${article.imageUrl}" alt="${article.altTxt}" />
+                    <img src="${article.imageUrl}" alt="${article.altTxt}">
                     <h3 class="productName">${article.name}</h3>
                     <p class="productDescription">${article.description}</p>
                 </article>
             </a>`;
-
-        }
 
     }
 }
