0018b30f98922d9d1a4fa58cce3bc0e09d93cb57
thomas.rogister <thomasrogister@protonmail.com>
2022-08-17 19:23:25 +0200

    simplification code

--- front/js/product.js
+++ front/js/product.js
@@ -9,46 +9,47 @@ console.log(id);
 fetch("http://localhost:3000/api/products")
     // DEMANDE DE RECEVOIR LA REPONSE EN J.SON
     .then((res) => res.json())
-    .then((articles) => {
-        products(articles);
+    .then((objetArticles) => {
+        products(objetArticles);
     })
 
     // AFFICHAGE D'UN MESSAGE EN CAS D'ERREUR
     .catch((err) => {
         document
             .querySelector(".item")
-            .innerHTML = `<h1> Ce site est en maintenance,</<br> nous nous excusons pour la gêne occasionné.</h1>`;
-        console.log(err + "empêche la réponse de s'afficher");
+            .innerHTML = "<h1> Ce site est en maintenance,</<br> nous nous excusons pour la gêne occasionné.</h1>";
+        console.log(err);
+
     });
 
 
-// CREATION OBJET CLIENT
-let articleClient = {};
-articleClient._id = id;
 
 // FUNCTION - AFFICHAGE PRODUIT
-function products(article) {
+function products(products) {
     // DECLARATION DES VARIABLE DES DIFFERENTS ELEMENTS
-    let img = document.querySelector("article div .item__img");
+    let image = document.querySelector("article div.item__img");
     let title = document.querySelector("#title");
     let price = document.querySelector("#price");
     let description = document.querySelector("#description");
     let colorOption = document.querySelector("#colors");
     // BOUCLE RECHERCHE CHOIX DE L'ARTICLE
-    for (let choice of article) {
+    const product = products.find(p => p._id === id);
+    console.log(product)
+    if (product) {
+        // console.log(product)
+        image.innerHTML = `<img src="${product.imageUrl}"alt="${product.altTxt}">`;
+        title.textContent = `${product.name}`;
+        price.textContent = `${product.price}`;
+        description.textContent = `${product.description}`;
+        // BOUCLE RECHERCHE DES COULEURS POUR CHAQUE PRODUIT
+        for (let color of product.colors) {
+            colorOption.innerHTML += `<option value="${color}">${color}</option>`;
 
-        if (id === choice._id) {
-            img.innerHTML = `<img scr="${choice.imageUrl}"alt="${choice.altTxt}">`;
-            title.textContent = `${choice.name}`;
-            price.textContent = `${choice.price}`;
-            description.textContent = `${choice.description}`;
-            // BOUCLE RECHERCHE DES COULEURS POUR CHAQUE PRODUIT
-            for (let color of choice.colors) {
-                colorOption.innerHTML += `<option value="${color}">${color}</option>`;
-            }
+
         }
     }
-    console.log("Affichage Ok");
+
+
 }
 
 
