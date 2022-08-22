9997afbd2a6db7b12ec55dd0d9ef781c0224d21f
thomas.rogister <thomasrogister@protonmail.com>
2022-08-22 13:59:30 +0200

    correction

--- front/js/cart.js
+++ front/js/cart.js
@@ -6,33 +6,34 @@ const basket = document.location.href;
 //  RÉCUPÉRATION DES PRODUITS DEPUIS L'API SI ON EST SUR LA PAGE PANIER
 if (basket.match("cart")) {
 
-    fetch("http://localhost:3000/api/products")
-        // DEMANDE DE RECEVOIR LA REPONSE EN J.SON
-        .then((res) => res.json())
-        .then((objetArticles) => {
-            console.log(objetArticles)
-            showBasket(objetArticles);
-        })
+  fetch("http://localhost:3000/api/products")
+    // DEMANDE DE RECEVOIR LA REPONSE EN J.SON
+    .then((res) => res.json())
+    .then((objetArticles) => {
+      console.log(objetArticles)
+      showBasket(objetArticles);
+    })
 
-        // AFFICHAGE D'UN MESSAGE EN CAS D'ERREUR
-        .catch((err) => {
-            document
-                .querySelector("#cartAndFormContainer")
-                .innerHTML = "<h1> Cette page est en maintenance,</<br> nous nous excusons pour la gêne occasionné.</h1>";
-            console.log(err);
+    // AFFICHAGE D'UN MESSAGE EN CAS D'ERREUR
+    .catch((err) => {
+      document
+        .querySelector("#cartAndFormContainer")
+        .innerHTML = "<h1> Cette page est en maintenance,</<br> nous nous excusons pour la gêne occasionné.</h1>";
+      console.log(err);
 
-        });
+    });
 
 } else {
-    console.log("page confirmation");
+  console.log("page confirmation");
 }
+
 function displayCartItem(article, objectArticles) {
-    const result = objectArticles.find(p => p._id === article._id);
+  const result = objectArticles.find(p => p._id === article._id);
 
 
 
 
-    document.querySelector("#cart__items").insertAdjacentHTML("beforeend", `<article class="cart__item" data-id="${article._id}" data-color="${article.color}">
+  document.querySelector("#cart__items").insertAdjacentHTML("beforeend", `<article class="cart__item" data-id="${article._id}" data-color="${article.color}">
     <div class="cart__item__img">
       <img src="${result.imageUrl}" alt="Photographie d'un canapé">
     </div>
@@ -55,52 +56,52 @@ function displayCartItem(article, objectArticles) {
   </article>`);
 }
 
-function showPrice(listLocalStorage, objectArticles) {
-    let totalQuantity = 0;
-    let totalPrice = 0;
-    listLocalStorage.map(o => {
-        const result = objectArticles.find(p => p._id === o._id);
-        const itemPrice = result.price;
-        const quantity = parseInt(o.quantity);
-        const total = quantity * itemPrice;
-        totalPrice = total + totalPrice;
-        totalQuantity = quantity + totalQuantity;
-    })
-    document.querySelector("#totalQuantity").innerHTML = `${totalQuantity}`;
-    document.querySelector("#totalPrice").innerHTML = `${totalPrice}`;
+function showPriceAndQuantity(listLocalStorage, objectArticles) {
+  let totalQuantity = 0;
+  let totalPrice = 0;
+  listLocalStorage.map(o => {
+    const result = objectArticles.find(p => p._id === o._id);
+    const itemPrice = result.price;
+    const quantity = parseInt(o.quantity);
+    const total = quantity * itemPrice;
+    totalPrice = total + totalPrice;
+    totalQuantity = quantity + totalQuantity;
+  })
+  document.querySelector("#totalQuantity").innerHTML = `${totalQuantity}`;
+  document.querySelector("#totalPrice").innerHTML = `${totalPrice}`;
 }
 
 
 
 // FUNCTION DETERMINE CONDITIONS AFFICHAGE BASKET
 function showBasket(objectArticles) {
-    // ON RECUP PANIER EN JSON
-    const basket = localStorage.getItem("storedBasket");
-    if (!basket) {
-        document.querySelector("#cart__items").innerHTML = "<h1>panier vide</h1>";
-    } else {
-        const basketItems = JSON.parse(basket);
-        basketItems.map(p => displayCartItem(p, objectArticles));
-        showPrice(basketItems, objectArticles);
-    }
+  // ON RECUP PANIER EN JSON
+  const basket = localStorage.getItem("storedBasket");
+  if (!basket) {
+    document.querySelector("#cart__items").innerHTML = "<h1>panier vide</h1>";
+  } else {
+    const basketItems = JSON.parse(basket);
+    basketItems.map(p => displayCartItem(p, objectArticles));
+    showPriceAndQuantity(basketItems, objectArticles);
+  }
 
 
 
-    // SI LE PANIER CONTIENT DEJA AU MOINS 1 ARTICLE
-    // for (let choice of basket) {
-    //     console.log(choice);
-    //     for (let p = 0, i = index.length; p < i; p++) {
-    //         if (choice._id === index[p]._id)
-    //             // CREATION VARIABLES POUR AJOUT DES DONNEES ARTICLE AU PANIER INDEXE
-    //             choice.name = index[p].name;
-    //         choice.prix = index[p].price;
-    //         choice.image = index[g].imageUrl;
-    //         choice.description = index[g].description;
-    //         choice.alt = index[g].altTxt;
-    //     }
-    // }
-    // AFFICHAGE DE PANIER (DONNEES ARTICLE AU DESSUS) + DONEES DU LOCALSTORAGE
-    // display(basket);
+  // SI LE PANIER CONTIENT DEJA AU MOINS 1 ARTICLE
+  // for (let choice of basket) {
+  //     console.log(choice);
+  //     for (let p = 0, i = index.length; p < i; p++) {
+  //         if (choice._id === index[p]._id)
+  //             // CREATION VARIABLES POUR AJOUT DES DONNEES ARTICLE AU PANIER INDEXE
+  //             choice.name = index[p].name;
+  //         choice.prix = index[p].price;
+  //         choice.image = index[g].imageUrl;
+  //         choice.description = index[g].description;
+  //         choice.alt = index[g].altTxt;
+  //     }
+  // }
+  // AFFICHAGE DE PANIER (DONNEES ARTICLE AU DESSUS) + DONEES DU LOCALSTORAGE
+  // display(basket);
 }
 //     else {
 //     // SI LE PANIER EST VIDE, ON AFFICHE SES INFOS ET UN MESSAGE
--- front/js/product.js
+++ front/js/product.js
@@ -113,7 +113,7 @@ let productToPush = [];
 
 function firstProduct() {
     console.log(productStored);
-    if (productStored === nul) {
+    if (productStored === null) {
         productChoiceClient.push(articleClient);
         console.log(articleClient);
         return (localStorage.storedBasket = JSON.stringify(productChoiseClient));
@@ -137,26 +137,6 @@ function addNewProduct(articleClient) {
         localStorage.setItem("storedBasket", JSON.stringify(newProducts));
     }
 }
-
-
-
-// productTemporary.push(articleClient);
-// productToPush = [...productStored, ...productTemporary];
-// // TRI DES TABLEAUX OBJET https://www.azur-web.com/astuces/javascript-trier-tableau-objet
-// produitsAPousser.sort(function triage(a, b) {
-//     if (a._id < b._id) return -1;
-//     if (a._id > b._id) return 1;
-//     if (a._id = b._id) {
-//         if (a.couleur < b.couleur) return -1;
-//         if (a.couleur > b.couleur) return 1;
-//     }
-//     return 0;
-//     });
-//     // INITIALISATION DE "productTemporary " APRES AVOIR ETE UTILISE
-//     productTemporary = [];
-//     // ENVOIE DU NEW ARTICLE => LOCALSTORAGE => "storedBasket" en JSON SRTINGIFY
-//     return (localStorage.storedBasket = JSON.stringify(productToPush));
-
 
 // AJOUT D'UN MÊME ARTICLE DANS LE TABLEAU, ELSE=> AJOUT DU TABLEAU || CREATION TABLEAU FIRST ARTICLE
 function basket() {
